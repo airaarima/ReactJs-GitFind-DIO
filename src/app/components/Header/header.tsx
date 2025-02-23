cea802d40790a00b3bcf854c2bc13ce618f605f3
Aira Arima <aira.arima@esx.com.br>
2025-02-23 16:17:09 -0300

    feat: cria componente header

---
+++ src/app/components/Header/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="w-full h-16 flex justify-center items-center bg-[#2D333B]">
+        <h1 className="text-4xl">GitFind</h1>
+    </header>
+  )
+}
