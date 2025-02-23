6291538c92d0c5f521b35da359744e33c2b80f40
Aira Arima <aira.arima@esx.com.br>
2025-02-23 16:16:40 -0300

    style: define fontes e variáveis de estilo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "GitFind",
+  title: "Git Find",
 };
 
 export default function RootLayout({
@@ -23,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
         {children}
       </body>
     </html>
